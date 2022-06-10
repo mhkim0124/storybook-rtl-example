480d76eb8d2c6f8e7dbf517ac3d31f2948bb22a1
mhkim0124 <mhkim@posicube.com>
2022-06-10 14:17:45 +0900

    update

--- src/components/Button/button.test.tsx
+++ src/components/Button/button.test.tsx
@@ -1,17 +1,19 @@
 import { render, screen, fireEvent } from "@testing-library/react";
 import { composeStories } from "@storybook/testing-react";
 
-import * as stories from "./Button.stories";
+import * as stories from "./button.stories";
 
 describe("Button", () => {
   const { DefaultButton, DisabledButton } = composeStories(stories);
 
-  it("renders the Button", async () => {
+  it("renders the Button", () => {
     // 버튼 컴포넌트를 렌더링합니다.
     render(<DefaultButton />);
+    const button = screen.getByText("default");
+    expect(button).toBeVisible();
   });
 
-  it("button click event", async () => {
+  it("button click event", () => {
     // jest에서 버튼의 onClick에 전달할 가짜함수를 생성합니다.
     const handleClick = jest.fn();
     render(<DefaultButton onClick={handleClick} />);
@@ -26,13 +28,13 @@ describe("Button", () => {
     expect(handleClick).toBeCalledTimes(1);
   });
 
-  it("disabled button click event", async () => {
+  it("disabled button click event", () => {
     const handleClick = jest.fn();
     render(<DisabledButton onClick={handleClick} />);
     const button = screen.getByText("disabled");
     fireEvent.click(button);
 
     // 버튼의 클릭이벤트가 발생하지 않았다면 테스트가 통과됩니다.
-    expect(handleClick).toBeCalledTimes(0);
+    return expect(handleClick).toBeCalledTimes(0);
   });
 });
